9ffd64e33c95e717a5982639f67d211fc09d0cd1
MaticeM <matice.morris@gmail.com>
2021-07-12 19:26:55 -0500

    stopped at Part 2, right before test it with SQL

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,23 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    public Employer() {
 
+    }
+
+    @NotBlank(message = "Please add a location.")
+    private String location;
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,16 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+
+@Entity
 public class Skill extends AbstractEntity {
+
+    public Skill() {
+
+    }
+
+    @NotBlank (message = "A description is required.")
+    public String description;
 
 }
