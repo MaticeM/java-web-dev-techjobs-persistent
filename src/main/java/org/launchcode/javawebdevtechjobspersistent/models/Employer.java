42d64f1e71ca427b68125de513e6d2c76ae9cdbd
MaticeM <matice.morris@gmail.com>
2021-07-15 19:23:54 -0500

    DONE!

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,17 +1,37 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
 
-    public Employer() {
-
-    }
-
     @NotBlank(message = "Please add a location.")
     private String location;
+
+    @OneToMany
+    @JoinColumn(name="employer_id")
+    private List<Job> jobs = new ArrayList<>();
+
+    public Employer() {
+    }
+
+    public Employer(String location) {
+        super();
+        this.location = location;
+    }
+
+//    public List<Job> getJobs() {
+//        return jobs;
+//    }
+//
+//    public void setJobs(List<Job> jobs) {
+//        this.jobs = jobs;
+//    }
 
     public String getLocation() {
         return location;
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,16 +1,42 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Skill extends AbstractEntity {
 
-    public Skill() {
+    @NotNull (message = "A description is required.")
+    private String description;
 
+    @ManyToMany(mappedBy = "skills")
+    private List<Job> jobs = new ArrayList<>();
+
+    public Skill() {
     }
 
-    @NotBlank (message = "A description is required.")
-    public String description;
+    public Skill(String description) {
+        super();
+        this.description = description;
+    }
 
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void addJob(Job job) {
+        this.jobs.add(job);
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
